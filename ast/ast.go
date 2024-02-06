4f23bac6840c7c5083813f612f8a627a9e92fc29
Fools <rewqppklt@163.com>
2024-02-06 15:42:00 +0000

    parse prefix expression

--- ast/ast.go
+++ ast/ast.go
@@ -124,3 +124,39 @@ func (es *ExpressionStatement) String() string {
 	}
 	return ""
 }
+
+// integer literal
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+func (il *IntegerLiteral) expressionNode() {}
+func (il *IntegerLiteral) TokenLiteral() string {
+	return il.Token.Literal
+}
+func (il *IntegerLiteral) String() string {
+	return il.Token.Literal
+}
+
+// prefix expression node
+type PrefixExpression struct {
+	Token    token.Token // prefix, for example "!"
+	Operator string      // the "-" or "!"
+	Right    Expression  // right expression
+}
+
+func (pe *PrefixExpression) expressionNode() {}
+func (pe *PrefixExpression) TokenLiteral() string {
+	return pe.Token.Literal
+}
+func (pe *PrefixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(pe.Operator)
+	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
