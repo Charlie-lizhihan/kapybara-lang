723b9461fe85649681bb3de58eb1693a85d9ab56
Fools <rewqppklt@163.com>
2024-02-22 17:10:40 +0000

    finish the evaluation of prefix expression

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,10 @@ import (
 	"kapybara-lang/object"
 )
 
+var TRUE = &object.Boolean{Value: true}
+var FALSE = &object.Boolean{Value: false}
+var NULL = &object.Null{}
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 
@@ -14,6 +18,11 @@ func Eval(node ast.Node) object.Object {
 		return Eval(node.Expression)
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	}
 
 	return nil
@@ -27,4 +36,45 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	} else {
+		return FALSE
+	}
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpressoin(right)
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperatorExpressoin(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
 }
