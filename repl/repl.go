46502c53a7df74fa11034769ad23763bb157b74a
Fools <rewqppklt@163.com>
2024-03-02 00:38:25 +0000

    add the identifier function

--- repl/repl.go
+++ repl/repl.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"kapybara-lang/evaluator"
 	"kapybara-lang/lexer"
+	"kapybara-lang/object"
 	"kapybara-lang/parser"
 )
 
@@ -14,7 +15,7 @@ const KAPYBARA = `／* ｀ ｴ´）www`
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-
+	env := object.NewEnvironment()
 	for {
 		fmt.Fprint(out, PROMPT)
 		scanned := scanner.Scan()
@@ -32,7 +33,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
